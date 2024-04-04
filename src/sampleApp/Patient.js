f837258ee4acd27de039d30b6f9aafe266e0386d
codefirst2022 <codefirst2022@gmail.com>
2024-04-04 19:36:58 +0530

    Patient crud

--- src/sampleApp/Patient.js
+++ src/sampleApp/Patient.js
@@ -32,6 +32,29 @@ const Patient = () => {
     const uppdateFormValue = (event,key) => {
         setPatientObj(oldObj => ({...oldObj,[key]:event.target.value}))
     }
+    const onEdit = (obj) => {
+        setPatientObj(obj)
+    } 
+    const updatedPatient = async () => {
+        const response =  await axios.put("https://freeapi.gerasim.in/api/HospitalAppointment/UpdatePatient",patientObj);
+        debugger;
+        if(response.data.result == true) {
+            alert('Patient Updated Success');
+            getAllPatient();
+        } else {
+            alert(response.data.message)
+        }
+    }
+    const onDelete = async (patientId) => {
+        const response =  await axios.delete("https://freeapi.gerasim.in/api/HospitalAppointment/DeletePatientByPatienId?patientId=" +patientId);
+        debugger;
+        if(response.data.result == true) {
+            alert('Patient Delete Success');
+            getAllPatient();
+        } else {
+            alert(response.data.message)
+        }
+    }
 
     return (
         <div>
@@ -52,6 +75,7 @@ const Patient = () => {
                                         <th>City</th>
                                         <th>Gender</th>
                                         <th>Age</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -64,6 +88,10 @@ const Patient = () => {
                                                 <td>{patient.city}</td>
                                                 <td>{patient.gender}</td>
                                                 <td>{patient.age}</td>
+                                                <td>
+                                                    <button className='btn btn-success' onClick={()=>{onEdit(patient)}}>Edit </button>
+                                                    <button className='btn btn-danger mx-2' onClick={()=>{onDelete(patient.patientId)}}>Delete </button>
+                                                </td>
                                             </tr>)
                                         })
                                     }
@@ -82,28 +110,29 @@ const Patient = () => {
                                 <div className='row'>
                                     <div className='col-6'>
                                         <label>Name</label>
-                                        <input type='text' onChange={(event)=>{uppdateFormValue(event,'name')}} className='form-control' />
+                                        <input type='text' value={patientObj.name} onChange={(event)=>{uppdateFormValue(event,'name')}} className='form-control' />
                                     </div>
                                     <div className='col-6'>
                                         <label>Mobile No</label>
-                                        <input type='text' onChange={(event)=>{uppdateFormValue(event,'mobileNo')}} className='form-control' />
+                                        <input type='text' value={patientObj.mobileNo} onChange={(event)=>{uppdateFormValue(event,'mobileNo')}} className='form-control' />
                                     </div>
                                     <div className='col-6'>
                                         <label>City</label>
-                                        <input type='text' onChange={(event)=>{uppdateFormValue(event,'city')}} className='form-control' />
+                                        <input type='text' value={patientObj.city} onChange={(event)=>{uppdateFormValue(event,'city')}} className='form-control' />
                                     </div>
                                     <div className='col-6'>
                                         <label>Age</label>
-                                        <input type='text' onChange={(event)=>{uppdateFormValue(event,'age')}} className='form-control' />
+                                        <input type='text' value={patientObj.age} onChange={(event)=>{uppdateFormValue(event,'age')}} className='form-control' />
                                     </div>
                                     <div className='col-6'>
                                         <label>Gender</label>
-                                        <input type='text' onChange={(event)=>{uppdateFormValue(event,'gender')}} className='form-control' />
+                                        <input type='text' value={patientObj.gender} onChange={(event)=>{uppdateFormValue(event,'gender')}} className='form-control' />
                                     </div> 
                                 </div>
                                 <div className='row'>
                                     <div className='col-12 text-center pt-3'>
                                         <button className='btn btn-success' onClick={createNewPatient}>Save Patient</button>
+                                        <button className='btn btn-primary' onClick={updatedPatient}>Update Patient</button>
                                     </div>
                                 </div>
                         </div>
